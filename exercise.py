707123c8e02bab8beffe1fffc4d741150ad18a98
Estudiantes <estudiantes@iMac-de-Pc-26.local>
2019-08-16 20:41:22 -0500

    Djangotest

---
+++ exercise.py
@@ -0,0 +1,12 @@
+import os
+from random import randint , uniform, random
+
+def Z():
+    AnsZ = randint (0,6)
+    return AnsZ
+    
+
+
+while i <= 6:
+    print (i)
+    i = i + 1 
--- numero.py
+++ numero.py
@@ -1,1 +1,7 @@
+N = 10
+i = 1
+
+while i <= N :
+    print (i)
+    i = i + 1 
 
